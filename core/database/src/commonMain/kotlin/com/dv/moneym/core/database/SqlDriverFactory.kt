7ceebe8983ecfcacb838829384914d655be32b82
davidvalic <valic.david@gmail.com>
2026-06-01 00:35:56 +0200

    Delete unused code

--- core/database/src/commonMain/kotlin/com/dv/moneym/core/database/SqlDriverFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.dv.moneym.core.database
-
-import app.cash.sqldelight.db.QueryResult
-import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.db.SqlSchema
-
-interface SqlDriverFactory {
-    fun create(schema: SqlSchema<QueryResult.Value<Unit>>, name: String): SqlDriver
-}
