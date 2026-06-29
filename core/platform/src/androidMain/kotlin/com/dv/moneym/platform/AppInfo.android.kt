3c31f49b2778fefde0e8b57ed491a3a92e567e1c
davidvalic <valic.david@gmail.com>
2026-06-30 01:23:53 +0200

    cleanup

--- core/platform/src/androidMain/kotlin/com/dv/moneym/platform/AppInfo.android.kt
+++ core/platform/src/androidMain/kotlin/com/dv/moneym/platform/AppInfo.android.kt
@@ -6,7 +6,6 @@ actual class AppInfo(private val context: Context) {
     actual val appName: String
         get() = context.packageManager.getApplicationLabel(context.applicationInfo).toString()
 
-    @Suppress("DEPRECATION")
     actual val versionName: String
         get() = context.packageManager.getPackageInfo(context.packageName, 0).versionName.orEmpty()
 }
